59abfbb439a9417544368395d0cc01dd1958e5fe
gtsitsvidze <gtsitsvidze97@gmail.com>
2020-10-28 00:58:42 +0400

    feat: moderation service

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map(comment => (
-    <li key={comment.key}>{comment.content}</li>
-  ))
+  const renderedComments = comments.map(comment => {
+    let content
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation'
+        break;
+    
+      default:
+        content = 'This comment has been rejected'
+        break;
+    }
+
+    return (
+      <li key={comment.key}>{content}</li>
+    )
+  })
+
   return (
     <ul>
       {renderedComments}
